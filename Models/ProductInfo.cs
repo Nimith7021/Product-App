19a048ba334a49446df51ff6ac7050eddd402fa2
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-12 18:54:02 +0530

    initial commit

---
+++ Models/ProductInfo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizedProductApp.Models
+{
+    internal class ProductInfo
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int Id { get; set; }
+
+        private double _discountPercentage;
+
+        private double _maxDiscountPercentage = 0.4;
+
+        private double _minDiscountPercentage = 0.2;
+        public double DiscountPercentage
+        {
+            set
+            {
+                if (value > _maxDiscountPercentage)
+                    _discountPercentage = _maxDiscountPercentage;
+                else if (value < _minDiscountPercentage)
+                    _discountPercentage = _minDiscountPercentage;
+                else
+                    _discountPercentage = value;
+            }
+
+            get
+            {
+                return _discountPercentage;
+
+            }
+
+        }
+
+        public ProductInfo() {
+
+            Console.WriteLine("Welcome to C#");
+        }
+
+        public ProductInfo(int id , string name , double price , double discountPercentage):this() //this() will call the default constructor which is defined above
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            DiscountPercentage = discountPercentage;
+        }
+
+        public double DiscountCalculation()
+        {
+            return (Price - (Price * DiscountPercentage));
+        }
+
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OptimizedProductApp.Models;
+
+namespace OptimizedProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductInfo product1 = new ProductInfo(1,"PS5",49500.50,0.35);
+            ProductInfo product2 = new ProductInfo(2,"X-Box",51300.45,0.20);  
+            
+            PrintDetails(product1);
+            Console.WriteLine("====================================>");
+            PrintDetails(product2);
+
+        }
+
+        static void PrintDetails(ProductInfo product) {
+
+            Console.WriteLine($" ID : {product.Id}\n Name:{product.Name}\n" +
+                $" Discount : {product.DiscountPercentage}\n" +
+                $" Price before discount :{product.Price}\n" +
+                $" Price after discount: {product.DiscountCalculation()}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*0.5 will be set to value and then value will be 
+                                                    will be validated through if else if conditions 
+                                                     and then the proper value will be set to 
+                                                         _discountPercentage*/ 
